bdb972e65b0600783158a86bb8a654e05525f721
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-10-05 11:15:59 +0200

    Add some base app layer plumbing

--- src/Return.Application/ServiceCollectionExtensions.cs
+++ src/Return.Application/ServiceCollectionExtensions.cs
@@ -5,18 +5,15 @@
 //  Project         : Return.Application
 // ******************************************************************************
 
-namespace Return.Application
-{
+namespace Return.Application {
     using System.Reflection;
     using AutoMapper;
     using Common.Behaviours;
     using MediatR;
     using Microsoft.Extensions.DependencyInjection;
 
-    public static class ServiceCollectionExtensions
-    {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
-        {
+    public static class ServiceCollectionExtensions {
+        public static IServiceCollection AddApplication(this IServiceCollection services) {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
--- src/Return.Persistence/ServiceCollectionExtensions.cs
+++ src/Return.Persistence/ServiceCollectionExtensions.cs
@@ -5,16 +5,13 @@
 //  Project         : Return.Persistence
 // ******************************************************************************
 
-namespace Return.Persistence
-{
+namespace Return.Persistence {
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Configuration;
 
-    public static class ServiceCollectionExtensions
-    {
-        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
-        {
+    public static class ServiceCollectionExtensions {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration) {
             services.AddDbContext<ReturnDbContext>(optionsAction: options =>
                 options.UseSqlServer(configuration.GetConnectionString("DbConnection")));
 
