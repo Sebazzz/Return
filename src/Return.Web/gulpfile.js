4b90a7fae85f67e7ad2a3b0c629ddce7b4443bfa
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-10-08 21:38:53 +0200

    Use gulp for basic SCSS compiling and asset management

---
+++ src/Return.Web/gulpfile.js
@@ -0,0 +1,34 @@
+/// <binding ProjectOpened='sass:watch' />
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function() {
+    return gulp
+        .src('./_scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(gulp.dest('./wwwroot/build/css/'));
+});
+
+gulp.task('copy-fonts', function() {
+    return gulp
+        .src('./node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('./wwwroot/build/fonts/'));
+});
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./_scss/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('clean', function(cb) {
+    del(['./wwwroot/build'], cb);
+});
+
+gulp.task('build', gulp.parallel('sass', 'copy-fonts'));
+
+gulp.task('default', gulp.series('clean', 'build'));
