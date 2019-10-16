492d0da4a396382ef13362cd17960165c9878f00
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2019-10-16 16:12:23 +0200

    Styling improvements for create page

--- src/Return.Application/Retrospectives/Commands/CreateRetrospective/CreateRetrospectiveCommandResponse.cs
+++ src/Return.Application/Retrospectives/Commands/CreateRetrospective/CreateRetrospectiveCommandResponse.cs
@@ -6,6 +6,7 @@
 // ******************************************************************************
 
 namespace Return.Application.Retrospectives.Commands.CreateRetrospective {
+    using System.Drawing;
     using Domain.ValueObjects;
     using QRCoder;
 
@@ -32,7 +33,7 @@ namespace Return.Application.Retrospectives.Commands.CreateRetrospective {
         public string ToBase64() {
             using var base64QrCode = new Base64QRCode(this._qrCodeData);
 
-            return "data:image/png;base64," + base64QrCode.GetGraphic(5);
+            return "data:image/png;base64," + base64QrCode.GetGraphic(5, Color.Black, Color.Transparent);
         }
     }
 }
