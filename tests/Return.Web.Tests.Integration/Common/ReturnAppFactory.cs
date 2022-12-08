044273dd77521e78009c7082dc268a4ff461ff83
Sebastiaan Dammann <sebastiaandammann@outlook.com>
2022-12-08 21:40:12 +0100

    Merge CustomWebApplicationFactory and ReturnAppFactory in prep of adopting Playwright

--- tests/Return.Web.Tests.Integration/Common/ReturnAppFactory.cs
+++ tests/Return.Web.Tests.Integration/Common/ReturnAppFactory.cs
@@ -11,32 +11,50 @@ using System;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using Application.Common.Abstractions;
+using Configuration;
 using Domain.Abstractions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 using Microsoft.Win32;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.Events;
 using Persistence;
+using Return.Application.App.Commands.SeedBaseData;
+using Return.Common;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
+using Microsoft.AspNetCore.Mvc.Testing;
 
-public sealed class ReturnAppFactory : CustomWebApplicationFactory<Startup> {
+public sealed class ReturnAppFactory : WebApplicationFactory<Startup> {
     private WebDriverPool _webDriverPool;
+    private IWebHost _webHost;
+
+    /// <summary>
+    /// Sqlite in-memory databases are killed as soon as the last referencing connection is killed. Therefore we always
+    /// want to hold on while the server might be used. In practice, this is per test fixture.
+    /// </summary>
+    private SqliteConnection _sqliteConnection;
 
     public ReturnAppFactory() {
         this._webDriverPool = new WebDriverPool(this.CreateWebDriver);
+        
+        this._sqliteConnection = new SqliteConnection(this.ConnectionString);
+        this._sqliteConnection.Open();
     }
 
-    protected override void Dispose(bool disposing) {
-        base.Dispose(disposing);
-
-        this._webDriverPool?.Dispose();
-        this._webDriverPool = null;
-    }
 
     public WebDriverContainer GetWebDriver() => new(this._webDriverPool.Get(), this);
 
@@ -137,11 +155,137 @@ public sealed class ReturnAppFactory : CustomWebApplicationFactory<Startup> {
 
     public IServiceScope CreateTestServiceScope() => this.Services.CreateScope();
 
-    protected override string ConnectionString { get; } = (new SqliteConnectionStringBuilder {
+    private string ConnectionString { get; } = (new SqliteConnectionStringBuilder {
         BrowsableConnectionString = true,
         Cache = SqliteCacheMode.Shared,
         Mode = SqliteOpenMode.Memory,
         ForeignKeys = true,
         DataSource = "testdb1"
     }).ToString();
+
+      [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Not necessary for tests")]
+    protected override void ConfigureWebHost(IWebHostBuilder builder) {
+        if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+        // Find free TCP port to configure Kestel on
+        IPEndPoint endPoint;
+        using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)) {
+            socket.Bind(new IPEndPoint(IPAddress.Loopback, 0));
+            socket.Listen(1);
+            endPoint = (IPEndPoint)socket.LocalEndPoint;
+        }
+
+        // Configure testing to use Kestel and test services
+        builder
+            .UseStaticWebAssets()
+            .ConfigureLogging(lb => {
+                lb.SetMinimumLevel(LogLevel.Trace);
+                lb.AddProvider(new TestContextLoggerProvider());
+
+                string logFileName = (TestContext.CurrentContext?.Test.ClassName ?? "test-log") + ".log";
+                lb.AddFile(Path.Join(Paths.TestArtifactDir, logFileName));
+            })
+            .ConfigureTestServices(services => {
+                // Add a database context using an in-memory database for testing.
+                services.RemoveAll<ReturnDbContext>();
+
+                services.AddScoped(sp => {
+                    DbContextOptions<ReturnDbContext> options = new DbContextOptionsBuilder<ReturnDbContext>()
+                        .UseSqlite(this.ConnectionString)
+                        .Options;
+
+                    var context = new ReturnDbContext(options);
+
+                    return context;
+                });
+                services.ChainInterfaceImplementation<IReturnDbContext, ReturnDbContext>();
+                services.ChainInterfaceImplementation<IReturnDbContextFactory, ReturnDbContext>();
+
+                services.Configure<ServerOptions>(s => {
+                    s.BaseUrl = "http://localhost:" + endPoint.Port + "/";
+                });
+            })
+            .UseKestrel(k => k.Listen(endPoint))
+            .UseEnvironment(environment: "Test");
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "It's fine for testing")]
+    public void InitializeBaseData() {
+        // Create a scope to obtain a reference to the database
+        using IServiceScope scope = this.Services.CreateScope();
+
+        IServiceProvider scopedServices = scope.ServiceProvider;
+        var context = scopedServices.GetRequiredService<ReturnDbContext>();
+        var logger = scopedServices.
+            GetRequiredService<ILogger<ReturnAppFactory>>();
+
+        // Ensure the database is created.
+        context.Database.EnsureCreated();
+
+        // ... Base seeding
+        try {
+            scope.SetNoAuthenticationInfo();
+
+            scope.Send(new SeedBaseDataCommand()).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+        catch (Exception ex) {
+            logger.LogError(ex, "An error occurred while migrating or initializing the database.");
+        }
+    }
+
+    protected override TestServer CreateServer(IWebHostBuilder builder) {
+        // See: https://github.com/aspnet/AspNetCore/issues/4892
+        this._webHost = builder.Build();
+
+        var testServer = new TestServer(new PassthroughWebHostBuilder(this._webHost));
+        var address = testServer.Host.ServerFeatures.Get<IServerAddressesFeature>();
+        testServer.BaseAddress = new Uri(address.Addresses.First());
+
+        return testServer;
+    }
+
+    private sealed class PassthroughWebHostBuilder : IWebHostBuilder {
+        private readonly IWebHost _webHost;
+
+        public PassthroughWebHostBuilder(IWebHost webHost) {
+            this._webHost = webHost;
+        }
+
+        public IWebHost Build() => this._webHost;
+
+        public IWebHostBuilder ConfigureAppConfiguration(Action<WebHostBuilderContext, IConfigurationBuilder> configureDelegate) {
+            TestContext.WriteLine($"Ignoring call: {typeof(PassthroughWebHostBuilder)}.{nameof(this.ConfigureAppConfiguration)}");
+            return this;
+        }
+
+        public IWebHostBuilder ConfigureServices(Action<WebHostBuilderContext, IServiceCollection> configureServices) {
+            TestContext.WriteLine($"Ignoring call: {typeof(PassthroughWebHostBuilder)}.{nameof(ConfigureServices)}");
+            return this;
+        }
+
+        public IWebHostBuilder ConfigureServices(Action<IServiceCollection> configureServices) {
+            TestContext.WriteLine($"Ignoring call: {typeof(PassthroughWebHostBuilder)}.{nameof(ConfigureServices)}");
+            return this;
+        }
+
+        public string GetSetting(string key) => throw new NotImplementedException();
+
+        public IWebHostBuilder UseSetting(string key, string value) {
+            TestContext.WriteLine($"Ignoring call: {typeof(PassthroughWebHostBuilder)}.{nameof(this.UseSetting)}({key}, {value})");
+            return this;
+        }
+    }
+
+    protected override void Dispose(bool disposing) {
+        base.Dispose(disposing);
+
+        if (disposing) {
+            this._webHost?.Dispose();
+            this._sqliteConnection?.Dispose();
+            this._sqliteConnection = null;
+
+            this._webDriverPool?.Dispose();
+            this._webDriverPool = null;
+        }
+    }
 }
